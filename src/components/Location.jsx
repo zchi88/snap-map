e0e6a2e8d13ffa9fe98950aac823fba3fa736818
Mike Chi <zhihaochi@gmail.com>
2019-04-22 21:10:55 -0400

    Creating a Location component to encapsulate location data.

---
+++ src/components/Location.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Location extends Component {
+    state = { 
+        
+     }
+    render() { 
+        console.log("Location rendering");
+        console.log("Location props: ", this.props )
+        return (  );
+    }
+}
+ 
+export default Location;

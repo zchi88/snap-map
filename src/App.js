ac82f2cfdc3b8a5342d973c5480393d64b22ae02
Mike Chi <zhihaochi@gmail.com>
2019-04-22 21:48:04 -0400

    Adding react-geocode library to translate string addresses into coordinates.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Geocode from "react-geocode";
+
 import "./App.css";
 import MapContainer from "./components/MapContainer";
 import SearchForm from "./components/SearchForm";
 
 const API = "https://www.easyfoodstamps.com/stores?";
 // const API = `https://www.easyfoodstamps.com/stores?latitude=${latitude}&longitude=${longitude}`;
+
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
 
 class App extends Component {
   state = {
@@ -18,6 +22,16 @@ class App extends Component {
 
   handleSearch = address => {
     console.log("Handle search called with address: ", address);
+    Geocode.fromAddress(address).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        console.log("Found corresponding coordinates: ");
+        console.log(lat, lng);
+      },
+      error => {
+        console.error(error);
+      }
+    );
   };
 
   render() {
